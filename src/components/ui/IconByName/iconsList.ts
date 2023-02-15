c4fdb5a3f08d69a2540904117b4581c77074ba8a
Raian Gil <raian.gdr@gmail.com>
2023-02-15 18:04:41 -0400

    agregando tailwind css y materialUI Icons

---
+++ src/components/ui/IconByName/iconsList.ts
@@ -0,0 +1,8 @@
+import { FaFileCsv } from 'react-icons/fa'
+
+export type IconsType = "fileCsv"
+
+export const iconsList = {
+  fileCsv: FaFileCsv,
+  
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import { TemplateForm } from '../features/Template';
 
 
 const Index = () =>{
-  const pjson = require('../../package.json');
-  console.log(`version: ${pjson.version}`, `JWT_KEY: ${process.env.JWT_KEY}`)
   return (
     <TemplateForm />
   )
